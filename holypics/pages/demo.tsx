296bbc05ce5ae6eb9d4f48158acaf212efabbf3d
Karamoko Israël Abdelaziz Axel <karamokoisrael@gmail.com>
2022-04-10 00:15:12 +0000

    don

--- holypics/pages/demo.tsx
+++ holypics/pages/demo.tsx
@@ -15,16 +15,20 @@ import {
   VStack,
   Button,
   AspectRatio,
+  Box,
 } from "native-base";
 import Layout from "../components/Layout";
 import useStore from "../context/store";
+import React from "react";
 
 // Start editing here, save and see your changes.
 export default function Demo() {
   const store = useStore();
   return (
     <Layout>
-      <h1>dpiojwei</h1>
+      <Box flex={1} bg="#fff" alignItems="center" justifyContent="center">
+        <Text>Open up App.js to start working on your app!</Text>
+      </Box>
     </Layout>
   );
 }
