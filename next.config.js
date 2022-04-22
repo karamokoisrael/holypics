75be1eca56a8d4b7717b739181f16c7a4a500661
Karamoko Israël Abdelaziz Axel <karamokoisrael@gmail.com>
2022-04-22 21:51:14 +0000

    ok

---
+++ next.config.js
@@ -0,0 +1,22 @@
+/** @type {import('next').NextConfig} */
+const env = {
+  global: {
+    DOCS_URL: "https://documenter.getpostman.com/view/16726391/UyrAEGLQ"
+  },
+  development: {
+    API_URL: "http://localhost:81",
+    PREDICTION_INTERVAL: 3000
+  },
+  production: {
+    API_URL: "https://api-holipics.karamokoisrael.tech",
+    PREDICTION_INTERVAL: 3000
+  }
+}
+
+const nextConfig = {
+  reactStrictMode: true,
+  env: process.env.NODE_ENV == "production" ? {...env.global, ...env.production} : {...env.global, ...env.development}
+  
+}
+
+module.exports = nextConfig
