8caa031d90f6e5ee93c1777e6b972935363bf49e
Karamoko Israël Abdelaziz Axel <karamokoisrael@gmail.com>
2022-05-31 19:43:43 +0000

    ...

--- backoffice/extensions/endpoints/ai/index.js
+++ backoffice/extensions/endpoints/ai/index.js
@@ -1,23 +1,4 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -33,7 +14,6 @@ const exceptions_1 = require("./../../helpers/exceptions");
 const directus_1 = require("directus");
 const fs = require('fs');
 const AdmZip = require("adm-zip");
-const tf = __importStar(require("@tensorflow/tfjs-node"));
 const utils_1 = require("../../helpers/utils");
 function default_1(router, { services, exceptions, getSchema, database, env }) {
     router.post('/predict/:id', (req, res) => __awaiter(this, void 0, void 0, function* () {
@@ -45,25 +25,37 @@ function default_1(router, { services, exceptions, getSchema, database, env }) {
                 filter: { name: { _eq: req.params.id } },
                 limit: 1
             });
-            const modelsData = yield database("models").whereIn("id", dataset.prediction_models);
-            for (let i = 0; i < modelsData.length; i++) {
-                const [file] = yield database("directus_files").where({ id: modelsData[i].tfjs_file });
-                const modelDataPath = `./uploads/tmp/${file.id}`;
-                if (!fs.existsSync(modelDataPath)) {
-                    fs.mkdirSync(modelDataPath);
-                    const zipFile = new AdmZip(`./uploads/${file.id}.zip`);
-                    zipFile.extractAllTo(modelDataPath, true);
-                }
-                const baseUrl = `${host}/file/tmp/download?path=${file.id}`;
-                const modelJsonUrl = `${baseUrl}/model.json`;
-                const modelWeightsUrl = `${host}/file/${modelDataPath.replace("./", "")}/group1-shard1of3.bin`;
-                const model = yield tf.loadLayersModel(
-                // "https://storage.googleapis.com/tfjs-models/tfjs/iris_v1/model.json"
-                modelJsonUrl);
-                console.log(model.summary());
-            }
-            dataset.prediction_models = modelsData;
+            console.log(dataset.prediction_models);
+            // const modelsData = await database("models").whereIn("id", dataset.prediction_models);
+            // console.log(modelsData);
+            // for (let i = 0; i < modelsData.length; i++) {
+            //         const [file] = await database("directus_files").where({ id: modelsData[i].tfjs_file })
+            //         const modelDataPath = `./uploads/tmp/${file.id}`;
+            //         if (!fs.existsSync(modelDataPath)) {
+            //                 fs.mkdirSync(modelDataPath);
+            //                 const zipFile = new AdmZip(`./uploads/${file.id}.zip`);
+            //                 zipFile.extractAllTo(modelDataPath, true);
+            //         }  
+            //         const baseUrl = `${host}/file/tmp/download?path=${file.id}`             
+            //         const modelJsonUrl = `${baseUrl}/model.json`
+            //         const modelWeightsUrl = `${host}/file/${modelDataPath.replace("./", "")}/group1-shard1of3.bin`
+            //         const model =  await tf.loadLayersModel(
+            //                 // "https://storage.googleapis.com/tfjs-models/tfjs/iris_v1/model.json"
+            //                 modelJsonUrl
+            //         );
+            //         console.log(model.summary());
+            // }
+            // dataset.prediction_models = modelsData;
             return res.json({ data: dataset });
+        }
+        catch (error) {
+            console.log(error);
+            return (0, exceptions_1.throwError)(res);
+        }
+    }));
+    router.get('/ok', (req, res) => __awaiter(this, void 0, void 0, function* () {
+        try {
+            return res.json({});
         }
         catch (error) {
             console.log(error);
--- backoffice/src/endpoints/ai/index.ts
+++ backoffice/src/endpoints/ai/index.ts
@@ -21,28 +21,43 @@ export default function (router: Router, { services, exceptions, getSchema, data
                                 filter: { name: { _eq: req.params.id } },
                                 limit: 1
                         })
-                        const modelsData = await database("models").whereIn("id", dataset.prediction_models);
-                        for (let i = 0; i < modelsData.length; i++) {
-                                const [file] = await database("directus_files").where({ id: modelsData[i].tfjs_file })
-                                const modelDataPath = `./uploads/tmp/${file.id}`;
-                                if (!fs.existsSync(modelDataPath)) {
-                                        fs.mkdirSync(modelDataPath);
-                                        const zipFile = new AdmZip(`./uploads/${file.id}.zip`);
-                                        zipFile.extractAllTo(modelDataPath, true);
-                                }  
-                                const baseUrl = `${host}/file/tmp/download?path=${file.id}`             
-                                const modelJsonUrl = `${baseUrl}/model.json`
-                                const modelWeightsUrl = `${host}/file/${modelDataPath.replace("./", "")}/group1-shard1of3.bin`
-                                const model =  await tf.loadLayersModel(
-                                        // "https://storage.googleapis.com/tfjs-models/tfjs/iris_v1/model.json"
-                                        modelJsonUrl
-                                );
+                        console.log(dataset.prediction_models);
+                        
+                        // const modelsData = await database("models").whereIn("id", dataset.prediction_models);
 
-                                console.log(model.summary());
+                        // console.log(modelsData);
+                        
+                        // for (let i = 0; i < modelsData.length; i++) {
+                        //         const [file] = await database("directus_files").where({ id: modelsData[i].tfjs_file })
+                        //         const modelDataPath = `./uploads/tmp/${file.id}`;
+                        //         if (!fs.existsSync(modelDataPath)) {
+                        //                 fs.mkdirSync(modelDataPath);
+                        //                 const zipFile = new AdmZip(`./uploads/${file.id}.zip`);
+                        //                 zipFile.extractAllTo(modelDataPath, true);
+                        //         }  
+                        //         const baseUrl = `${host}/file/tmp/download?path=${file.id}`             
+                        //         const modelJsonUrl = `${baseUrl}/model.json`
+                        //         const modelWeightsUrl = `${host}/file/${modelDataPath.replace("./", "")}/group1-shard1of3.bin`
+                        //         const model =  await tf.loadLayersModel(
+                        //                 // "https://storage.googleapis.com/tfjs-models/tfjs/iris_v1/model.json"
+                        //                 modelJsonUrl
+                        //         );
+
+                        //         console.log(model.summary());
                                 
-                        }
-                        dataset.prediction_models = modelsData;
+                        // }
+                        // dataset.prediction_models = modelsData;
                         return res.json({ data: dataset });
+                } catch (error) {
+                        console.log(error);
+                        return throwError(res);
+                }
+
+        });
+
+        router.get('/ok', async (req: Request, res: Response) => {
+                try {
+                        return res.json({})
                 } catch (error) {
                         console.log(error);
                         return throwError(res);
