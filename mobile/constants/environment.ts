3132ed074d275d5943b9c41c95bb8af23b706b8b
Karamoko Israël Abdelaziz Axel <karamokoisrael@gmail.com>
2022-06-10 13:41:43 +0000

    ...

--- mobile/constants/environment.ts
+++ mobile/constants/environment.ts
@@ -1,5 +1,5 @@
 export default {
-    apiUrl: 'http://192.168.1.3:8055',
+    apiUrl: process.env.NODE_ENV === "development" ? 'http://192.168.1.3:8055' : 'api-holipics.karamokoisrael.tech',
     // apiUrl: 'https://mon-immo.laboutiik.ci',
-    environment: 'development'
+    environment: process.env.NODE_ENV
 }
