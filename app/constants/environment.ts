a2676d25c08c7568637277f551789a81379dd327
Karamoko Israël Abdelaziz Axel <karamokoisrael@gmail.com>
2022-08-03 18:10:55 +0000

    ...

--- app/constants/environment.ts
+++ app/constants/environment.ts
@@ -1,5 +1,5 @@
 export default {
     // apiUrl: process.env.NODE_ENV === "development" ? 'http://192.168.1.3:8055' : 'api-holipics.karamokoisrael.tech',
-    apiUrl: process.env.NODE_ENV === "development" ? 'http://192.168.1.5:8055' : 'https://api-holipics.karamokoisrael.tech',
+    apiUrl: process.env.NODE_ENV === "development" ? 'http://192.168.1.5:8055' : 'https://mgx-api.karamokoisrael.tech',
     environment: process.env.NODE_ENV
 }
