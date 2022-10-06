49babb1abdceba5d2577802a70402a0e260ab86c
Karamoko Israël Abdelaziz Axel <karamokoisrael@gmail.com>
2022-10-06 07:27:03 +0000

    removing request  break

--- test/index.js
+++ test/index.js
@@ -1,10 +1,3 @@
-const hf = require('huggingface-api')
+const predict = async ()=>{
 
-hf.request({
-	text: 'My name is Jeff and',
-	model: 'EleutherAI/gpt-neo-2.7B',
-	api_key: 'api_[API KEY REDACTED]',
-	return_type: 'STRING'
-}).then((data) => {
-	console.log(data)
-})
+}
