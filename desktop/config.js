c41db0abeca92c0abbbaf871bc338cd5eef414dc
Karamoko Israël Abdelaziz Axel <karamokoisrael@gmail.com>
2022-10-06 03:13:01 +0000

    adding electron

---
+++ desktop/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  appUrl: "http://localhost:19006",
+  appIndexFile: "website/index.html",
+  windowWidth: 800,
+  windowHeight: 600,
+};
--- desktop/main.js
+++ desktop/main.js
@@ -1,19 +1,53 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
-const path = require('path')
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+const config = require("./config");
+const isDev = require("electron-is-dev");
 
-function createWindow () {
+async function createWindow() {
+  // Create splash browser window.
+
+  const startUrl = url.format({
+    pathname: path.join(__dirname, config.appIndexFile),
+    protocol: "file:",
+    slashes: true,
+  });
+
+  const splash = new BrowserWindow({
+    width: config.windowWidth,
+    height: config.windowHeight,
+    transparent: true,
+    frame: false,
+    alwaysOnTop: true,
+  });
+
+  splash.loadFile("splash.html");
+  splash.center();
+
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: config.windowWidth,
+    height: config.windowHeight,
+    show: false,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+  mainWindow.center();
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('../app/website/index.html')
+  // Load app
+  await mainWindow.loadURL(
+    isDev
+      ? config.appUrl
+      : `file://${path.join(__dirname, config.appIndexFile)}`
+  );
+  if (isDev) {
+    mainWindow.webContents.openDevTools({ mode: "detach" });
+  }
+  // Close loading screen and show directus app
+  splash.close();
+  mainWindow.show();
+  mainWindow.focus();
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
@@ -23,21 +57,21 @@ function createWindow () {
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  createWindow()
+  createWindow();
 
-  app.on('activate', function () {
+  app.on("activate", function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
-})
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") app.quit();
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
